2dc7926e0d926d0b7a2cee97da29bde50c44319b
salah eddine JOUHAINI <126112731+salahjouhaini@users.noreply.github.com>
2024-04-16 00:51:35 +0000

    initialisation de projet

---
+++ src/main/java/com/example/automates1/Automates1Application.java
@@ -0,0 +1,13 @@
+package com.example.automates1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Automates1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Automates1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/automates1/Automates1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automates1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Automates1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
